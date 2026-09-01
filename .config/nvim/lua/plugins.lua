8661aa0f34863cbe0b439a061589875a250b56ca
Josh Lester <josh@joshualester.com>
2026-09-01 15:41:22 -0700

    nvim changes

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-telescope/telescope.nvim',
+})
+
+require('telescope').setup({
+  defaults = {
+    -- config
+  }
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
--- .zshrc
+++ .zshrc
@@ -1,2 +1,5 @@
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias vim='nvim'
+alias k='kubectl'
+export PATH="$PATH:/home/joshu/.dotnet/tools"
+export PATH="$HOME/.local/bin:$PATH"
