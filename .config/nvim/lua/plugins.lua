2e1565f72bf2024d69f27344db1ec366fda83569
Josh Lester <josh@joshualester.com>
2026-09-15 15:14:15 -0700

    lazygit shtuff + nvim readme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,3 +15,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+
+-- terminal keymaps
+vim.keymap.set("n", "<leader>gg", function()
+  vim.cmd("vsplit | terminal lazygit")
+  vim.cmd("startinsert")
+end, { desc = "Open lazygit" })
